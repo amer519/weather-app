ac5bd313b096be3ac7b50dd0f14d67bcad111fd1
amer519 <afahmy519@gmail.com>
2021-02-04 12:53:01 -0500

    app.js adjustments

--- src/App.js
+++ src/App.js
@@ -33,6 +33,14 @@ function App() {
           <div className='location'>New York City, US</div>
           <div className='date'>{dateBuilder(new Date())}</div>
         </div>
+        <div classname='weather-box'>
+          <div className='temp'>
+            15c
+          </div>
+          <div className='weather'>
+            Sunny
+          </div>
+        </div>
       </main>
     </div>
   );
