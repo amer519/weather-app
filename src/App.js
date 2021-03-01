aa0bb6894f49700556171e90fc4684170d73eddc
amer519 <afahmy519@gmail.com>
2021-02-28 23:12:13 -0500

    logic for temp

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ function App() {
           <div className='date'>{dateBuilder(new Date())}</div>
         </div>
         <div className='weather-box'>
-          <div className='temp'>15c</div>
+          <div className='temp'>{Math.round(weather.main.temp)}°C</div>
           <div className='weather'>Sunny</div>
           </div>
         </div>
